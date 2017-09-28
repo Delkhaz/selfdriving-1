da7fb56614ed76849a7973dc811329df9bbe38ac
sremedios <sam.remedios@gmail.com>
2017-09-28 12:08:18 -0500

    trash

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ main.py will manage the image capture and instruction set
 
 import cv2
 import numpy as np
-from PIL import Image
 
 # init camera
 camera = cv2.VideoCapture(0)
@@ -22,12 +21,9 @@ def get_image():
 
 while True:
 	im = get_image()
-	#im.show()	
 
-	# trying to display image via PIL instead.
-	# currently not working
-	im_PIL = Image.new("RGB", (480,640), "white")
-	im_PIL.putdata(im)
-	im_PIL.show()
-	#cv2.imshow('ImageWindow', im)
+	cv2.imshow('ImageWindow', im)
 	cv2.waitKey(0)
+	break
+im[np.where(im[::0]<=175)]=0
+cv2.imshow("newim", im)
